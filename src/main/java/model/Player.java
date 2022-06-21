72eced518e764c4251e667ff3961a290a07ecf57
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-06-21 13:05:51 +0200

    Player class modified to have preliminary attributes and builder. Also builder includes a evaluator to assign Color based on idNumber and shows error message if color has not been assigned correctly

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,35 @@
 package model;
 
 public class Player {
+
+    private int idNumber;
+    private String name;
+    private Color color;
+
+    public Player(int idNumber, String name) {
+        this.idNumber = idNumber;
+        this.name = name;
+        switch (idNumber){
+            case 1:
+                color = Color.BLUE;
+                break;
+            case 2:
+                color = Color.YELLOW;
+                break;
+            case 3:
+                color = Color.GREEN;
+                break;
+            case 4:
+                color = Color.RED;
+                break;
+            default:
+                color = null;
+                break;
+        }
+
+        if(color == null){
+            System.out.println("ERROR: No Color assigned to player");
+        }
+
+    }
 }
