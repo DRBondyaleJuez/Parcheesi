094ed2069a8cfd6dd053dd26765fd1377e1adb6b
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-06-21 13:04:00 +0200

    Square class has been added preliminary attributes and builder. Possibility of this class becoming an interface for normalsquare and final square. Requires thinking

--- src/main/java/model/Square.java
+++ src/main/java/model/Square.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Square {
+
+    private int squareNumber;
+    private Piece currentPiece;
+    private boolean isSafe;
+    private boolean isStart;
+    private boolean isFinal;
+    private Square nextSquare;
+
 }
