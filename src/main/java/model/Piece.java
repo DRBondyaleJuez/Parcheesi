e4e9657711a8c99ea436b1ed45c9d6515146d1e0
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-06-21 13:02:10 +0200

    Piece model has been added preliminary attributes and builder

--- src/main/java/model/Piece.java
+++ src/main/java/model/Piece.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Piece {
+
+    private int stepCounter;
+    private int finalStepCounter;
+    private int player;
+    private Square currentSquare;
+
+    public Piece(int player, Square currentSquare) {
+        this.player = player;
+        this.currentSquare = currentSquare;
+        stepCounter = 0;
+        finalStepCounter = 0;
+    }
+
+
+
 }
