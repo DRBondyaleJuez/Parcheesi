adc0c1670b57f0d631ef3430f3cf29748b35b819
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-06-23 16:15:08 +0200

    Preliminary methods of the Piece class created

--- src/main/java/model/Piece.java
+++ src/main/java/model/Piece.java
@@ -14,6 +14,45 @@ public class Piece {
         finalStepCounter = 0;
     }
 
+    public int getPlayer() {
+        return player;
+    }
+
+    public Square getCurrentSquare() {
+        return currentSquare;
+    }
+
+    public void setCurrentSquare(Square currentSquare) {
+        this.currentSquare = currentSquare;
+    }
+
+    public void Move(int steps){
+        stepCounter = stepCounter + steps;
+        if(stepCounter > 60){
+            int finalSteps = stepCounter - 60;
+            stepCounter = 60;
+            finalMove(finalSteps);
+        }
+    }
+
+    public int finalMove(int steps){
+        finalStepCounter = finalStepCounter + steps;
+        //If the steps go over the final square
+        if(finalStepCounter>8){
+            finalStepCounter = 8 - (finalStepCounter-8);
+            if(finalStepCounter<1){finalStepCounter = 1;}
+            return finalStepCounter;
+        }
+        //If it reaches the final square
+        if(finalStepCounter==8){
+            finalStepCounter = 10;
+            return finalStepCounter;
+        }
+        //If the steps don't reach the final square
+        return finalStepCounter;
+
+    }
+
 
 
 }
