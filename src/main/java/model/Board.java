c8cb9384fe52ba1e674efdb85d534f6794d77c44
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-06-23 19:58:01 +0200

    Creation of building method for Board Class. This builder creates new Square and FinalSquare objects in an array and also connects them between each other.

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -1,4 +1,39 @@
 package model;
 
 public class Board {
+
+    Square[] boardSquares;
+    FinalSquare[][] finalSquaresBoard;
+    int[] housePieces; //This is the pieces not played by each player
+
+    public Board() {
+        //Setting initial configuration of houses
+        housePieces = new int[4];
+        for (int numberOfPieces:housePieces) {
+            numberOfPieces=4;
+        };
+
+        //Building boardSquares
+        boardSquares = new Square[60];
+        for (int i = 0; i < boardSquares.length; i++) {
+            Square currentSquare = new Square(i+1);
+            if(i==0){
+                continue;
+            }
+            if(i==59){
+                currentSquare.setNextSquare(boardSquares[0]);
+                continue;
+            }
+            boardSquares[i-1].setNextSquare(currentSquare);
+        }
+
+        //Building finalSquaresBoard
+        finalSquaresBoard = new FinalSquare[8][4];
+        for (int player = 0; player < finalSquaresBoard[0].length; player++) {
+            for (int i = 0; i < finalSquaresBoard.length; i++) {
+                FinalSquare currentFinalSquare = new FinalSquare(i+1,null,player);
+            }
+        }
+    }
+    
 }
