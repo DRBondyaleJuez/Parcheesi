eb49e49c6aaef8dd8142306f416c4981ac868a04
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-06-29 14:03:08 +0200

    Piece class was modified to better suit the requirements of the method of the controller for piece movement. These changes include a board position caculator based on stepcounter and player, an improved final step counter, a series of getter which initially were not considered necesary

--- src/main/java/model/Piece.java
+++ src/main/java/model/Piece.java
@@ -1,17 +1,21 @@
 package model;
 
+import model.Square.Square;
+
 public class Piece {
 
     private int stepCounter;
     private int finalStepCounter;
     private int player;
     private Square currentSquare;
+    private int boardPosition;
 
-    public Piece(int player, Square currentSquare) {
+    public Piece(int player, Square newSquare) {
         this.player = player;
-        this.currentSquare = currentSquare;
+        currentSquare = newSquare;
         stepCounter = 0;
         finalStepCounter = 0;
+        boardPosition = boardPositionCalculator(stepCounter);
     }
 
     public int getPlayer() {
@@ -26,33 +30,71 @@ public class Piece {
         this.currentSquare = currentSquare;
     }
 
-    public void Move(int steps){
+    public int getStepCounter() {
+        return stepCounter;
+    }
+
+    public int getBoardPosition() {
+        return boardPosition;
+    }
+
+    public void Move(int steps, Square newSquare){
+        //Changing the step counter with possible nuance if final squares are reached
         stepCounter = stepCounter + steps;
         if(stepCounter > 60){
             int finalSteps = stepCounter - 60;
             stepCounter = 60;
-            finalMove(finalSteps);
+            finalStepCounter = finalMoveCalculator(finalSteps);
         }
+        //Change the square where the piece is currently after moving
+        setCurrentSquare(newSquare);
     }
 
-    public int finalMove(int steps){
-        finalStepCounter = finalStepCounter + steps;
-        //If the steps go over the final square
-        if(finalStepCounter>8){
-            finalStepCounter = 8 - (finalStepCounter-8);
-            if(finalStepCounter<1){finalStepCounter = 1;}
-            return finalStepCounter;
+    public int finalMoveCalculator(int steps){
+        int tempFinalStepCounter = finalStepCounter;
+        tempFinalStepCounter = tempFinalStepCounter + steps;
+        //If the steps go over the end square
+        if(tempFinalStepCounter>8){
+            tempFinalStepCounter = 8 - (tempFinalStepCounter-8);
+            if(tempFinalStepCounter<1){tempFinalStepCounter = 1;}
+            return tempFinalStepCounter;
         }
-        //If it reaches the final square
+        //If it reaches the end square
         if(finalStepCounter==8){
             finalStepCounter = 10;
             return finalStepCounter;
         }
-        //If the steps don't reach the final square
+        //If the steps don't reach the end square
         return finalStepCounter;
 
     }
 
+    public int boardPositionCalculator(int currentStepCounter){
+        int startingPosition = 0;
+        int newBoardPosition = currentStepCounter;
+        switch(player){
+            case 1:
+                startingPosition = 4;
+                break;
+            case 2:
+                startingPosition = 19;
+                break;
+            case 3:
+                startingPosition = 24;
+                break;
+            case 4:
+                startingPosition = 39;
+                break;
+            default:
+                break;
+        }
+        newBoardPosition = startingPosition + currentStepCounter;
+        return newBoardPosition;
+    }
+
+    public int getFinalStepCounter(){
+        return finalStepCounter;
+    }
 
 
 }
