d3d7fac9a434bd78a828ba9931a7fe606a3d98b4
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-07-03 00:05:45 +0200

    Minor changes to better fit the processes of piece returning home either for punishment or capture

--- src/main/java/model/Board.java
+++ src/main/java/model/Board.java
@@ -58,29 +58,29 @@ public class Board {
 
     //Piece exiting houseSquares and entering normalSquares
     public boolean pieceExitsHouse(int player){
-        int position = player-1; // correction due to player number and position in array being different
+        int playerPosition = player-1; // correction due to player number and position in array being different
 
         // First checking for pieces in house ready to exit. If this number is 0 no piece can enter and the 5 has to be used for movement i.e. return false to controller
-        if(housePieces[position]<1){
+        if(housePieces[playerPosition]<1){
             return false;
         }
 
         //Declaring starting square for the corresponding player
         NormalSquare playerStartingNormalSquare = boardNormalSquares[startSquares[player-1]-1];
-        if (playerStartingNormalSquare.getIsStart() == player){ //Perhaps this is unnecessary but check that the square is confirmed to be a start square
-            //Check if the square is blocked by the presence of two pieces
-            if(playerStartingNormalSquare.isBlocked()){
-                return false;
-            }
 
-            //Put piece in starting square
-            playerStartingNormalSquare.setCurrentPlayerPiece(player);
-            int pieceNumber = 4 - housePieces[position];
-            pieces[position][pieceNumber].setCurrentSquare(playerStartingNormalSquare);
-            housePieces[position]--;
-            return true;
+        //Check if the square is blocked by the presence of two pieces
+        if(playerStartingNormalSquare.isBlocked()){
+            return false;
         }
-        return false;
+
+        //Put piece in starting square
+        playerStartingNormalSquare.setCurrentPlayerPiece(player);
+        int pieceNumber = 4 - housePieces[playerPosition];
+        Piece enteringPiece = pieces[playerPosition][pieceNumber];
+        enteringPiece.move(1,playerStartingNormalSquare);
+        housePieces[playerPosition]--;
+        return true;
+
     }
 
 
